96ee12ff4bbd6a9b1d3dfbe5eb6144bb62e866f2
eliot <bengattathadjali@gmail.com>
2019-03-07 19:58:16 +0100

    [master]: new req

---
+++ README.md
@@ -0,0 +1,6 @@
+To run the container:
+    $ docker-compose run -d
+To stop :
+    $ docker stop $(docker ps -aq)
+To remove :
+    $ docker rm $(docker ps -aq)
--- requirements.sh
+++ requirements.sh
@@ -2,6 +2,7 @@ echo '          #####################################'
 echo '          Configuration to run odoo with docker'
 echo '          #####################################'
 printf "\n\n\n"
+#Install docker
 sudo apt-get update
 sudo apt-get install \
     apt-transport-https \
@@ -19,6 +20,9 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker your-user
+#image odoo and postgress
 sudo docker pull odoo:11
-sudo docker pull postgres:9.6
+sudo docker pull postgres:9.6v
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 sudo apt-get install python3.5
