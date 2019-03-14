eade9f632a08c1b4295d9e75faa71b11a362aa12
bendjibenz <bendjelloulnoureddine@gmail.com>
2019-03-14 16:33:46 +0100

    [master]: correction of requirements file

--- requirements.sh
+++ requirements.sh
@@ -1,28 +1,37 @@
+#!/bin/bash
 echo '          #####################################'
 echo '          Configuration to run odoo with docker'
 echo '          #####################################'
 printf "\n\n\n"
+
 #Install docker
+
 sudo apt-get update
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg2 \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+# 1 er methode pour installer docker
+# sudo apt-get install \
+#     apt-transport-https \
+#     ca-certificates \
+#     curl \
+#     gnupg2 \
+#     software-properties-common
+
+# curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+# sudo add-apt-repository \
+#    "deb [arch=amd64] https://download.docker.com/linux/debian \
+#    $(lsb_release -cs) \
+#    stable"
+# sudo apt-get update
+# sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+# 2 em methode pour installer docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
-sudo usermod -aG docker your-user
+sudo usermod -aG docker $USER
+
+# Install docker compose
+sudo apt install python-pip
+sudo pip install docker-compose
+
 #image odoo and postgress
-sudo docker pull odoo:11
-sudo docker pull postgres:9.6v
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo apt-get install python3.5
+sudo docker pull odoo:11.0
+sudo docker pull postgres:9.6
